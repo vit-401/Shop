7594e849d5436a3bb7830d1a1f67267f6b84f0fe
Vitaliy <60542954+vit-401@users.noreply.github.com>
2020-04-04 16:39:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+$(function() {
+  $('.bt').on('click', function() {
+
+    let val1, val2;
+    val1 = $('#pn-1').val();
+    val2 = $('#pn-2').val();
+    $('.form-1').append('<div class="td "><div class="x">❌</div><h2 class="h2">' + val1 + '</h2><hr><span class="span">' + val2 + '</span></div>');
+
+
+    $('.x').on('click', function() {
+      $('.td').remove();
+
+    });
+
+
+
+  });
+});
